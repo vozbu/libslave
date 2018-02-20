6d14e81fa326a677527b543c40c80a82c7af6cd9
Valeriy Bykov <v.bykov@corp.mail.ru>
2018-02-20 13:55:48 +0300

    [!] slave: db_filler moved to std::chrono

--- test/db_filler.cpp
+++ test/db_filler.cpp
@@ -1,5 +1,8 @@
 #include <getopt.h>
+
+#include <chrono>
 #include <iostream>
+
 #include "nanomysql.h"
 #include "Slave.h"
 
@@ -43,12 +46,9 @@ void createTable(const nanomysql::mysql_conn_opts& opts, const std::string& aTab
         sQuery += aQueryPart + ",";
     sQuery += aQueryPart;
 
-    timespec start, stop;
-    long long diff;
-
     std::cerr << ::time(NULL) << " Inserting data..." << std::endl;
     time_t sLastOutputTime = 0;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    auto start = std::chrono::steady_clock::now();
     for (unsigned i = bulk_count; i > 0; --i)
     {
         const time_t ct = ::time(nullptr);
@@ -59,21 +59,21 @@ void createTable(const nanomysql::mysql_conn_opts& opts, const std::string& aTab
         }
         conn.query(sQuery);
     }
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
+    auto stop = std::chrono::steady_clock::now();
     std::cerr << std::endl;
 
-    diff = (stop.tv_sec - start.tv_sec) * 1000000000 + (stop.tv_nsec - start.tv_nsec);
-    std::cout << "Inserted in " << (diff / 1e9) << " seconds\n";
+    auto diff = stop - start;
+    std::cout << "Inserted in " << (std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() / 1e3) << " seconds\n";
 
     sBinlogPos = sSlave.getLastBinlogPos();
     std::cout << "Master pos after insert is " << sBinlogPos << std::endl;
 
     std::cerr << ::time(NULL) << " Updating data..." << std::endl;
-    clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+    start = std::chrono::steady_clock::now();
     conn.query(aUpdateQuery);
-    clock_gettime(CLOCK_MONOTONIC_RAW, &stop);
-    diff = (stop.tv_sec - start.tv_sec) * 1000000000 + (stop.tv_nsec - start.tv_nsec);
-    std::cout << "Updated in " << (diff / 1e9) << " seconds\n";
+    stop = std::chrono::steady_clock::now();
+    diff = stop - start;
+    std::cout << "Updated in " << (std::chrono::duration_cast<std::chrono::milliseconds>(diff).count() / 1e3) << " seconds\n";
 
     sBinlogPos = sSlave.getLastBinlogPos();
     std::cout << "Master pos after update is " << sBinlogPos << std::endl;
